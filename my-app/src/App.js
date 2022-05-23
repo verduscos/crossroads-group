9fc00762fbc3a7b01ef5edc897db127f164df4ab
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-05-23 11:24:59 -0700

    created react app, fetching commit history in app.js via github api

---
+++ my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect} from 'react'
+
+function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/verduscos/crossroads-group/commits")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.commit.author.name}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default App;
